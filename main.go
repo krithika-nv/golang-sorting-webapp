f7d114c7739776e8c5994e0fbb0175a848fac8ac
krithika-nv <nkrit@amazon.com>
2021-03-21 15:48:08 +0530

    logging

--- main.go
+++ main.go
@@ -35,10 +35,9 @@ func init() {
 
 
 func increment(res http.ResponseWriter, req *http.Request) {
-	if strings.Contains(req.URL.String(),"favicon.ico"){ //Check to avoid counting /favicon.ico request
+	if strings.Contains(req.URL.String(),"favicon.ico") { //Check to avoid counting /favicon.ico request
 	    log.Info("Ignoring favicon requests in counter metrics")
-	}
-	else{
+	} else {
 	log.Info("Incoming HTTP request")
 	fmt.Fprintf(res, "%s", "<table border=\"1\">")
 	fmt.Fprintf(res, "%s", "<th>Header Field</th>")
